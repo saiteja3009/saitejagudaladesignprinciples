5eed3a4ed75e9939c01443b16f641b5e82922e61
saiteja3009 <54467330+saiteja3009@users.noreply.github.com>
2020-02-13 19:47:16 +0530

    Delete WindowClosingAdapter.java

--- WindowClosingAdapter.java
+++
@@ -1,8 +0,0 @@
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
-public class WindowClosingAdapter extends WindowAdapter {
-    public void windowClosing(WindowEvent e) {
-        System.exit(0);
-    }
-}
